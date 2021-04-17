073f2790e0ae85558b8bf12650b6adb8a2dddcf1
Danny <behappydanny1@gmail.com>
2021-04-17 14:43:50 +0800

    update playing buttons

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,13 @@
 
     <main>
         <section class="player">
-          <h2 class = "song-title">Test</h2>
+          <h2 class = "song-title">{{current.title}} - <span>{{current.artist}}</span></h2>
+          <div class="contro">
+            <button class="prev">Prev</button>
+            <button class="play" v-if="!isPlaying">Play</button>
+            <button class="pause" v-else>Pause</button>
+            <button class="next">Next</button>
+          </div>
         </section>
     </main>
   </div>
@@ -15,7 +21,32 @@
 <script>
 
 export default {
-  name: 'app'
+  name: 'app',
+  data(){
+    return{
+      current:{},
+      index: 0,
+      isPlaying:false,
+      songs:[
+        {
+          title: 'Hold on',
+          artist: 'Justin Bieber',
+          src: require('./assets/Justin Bieber - Hold On.mp3')
+        },
+        {
+        title:'BANKRUPT',
+        artist:'Russ',
+        src: require('./assets/Russ - BANKRUPT.mp3')
+        }
+      ],
+      player:new Audio()
+    }
+  },
+  created(){
+    this.current = this.songs[this.index];
+    this.player.src = this.current.src;
+    //this.player.play();
+    }
  
   
 }
